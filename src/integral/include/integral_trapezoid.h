ac1092260871450556a5a52270ce51cb220b74f1
Alexander Feodorov <webmaster@unixcomp.org>
2019-10-25 23:43:22 +0300

    Add interpolation function

--- src/integral/include/integral_trapezoid.h
+++ src/integral/include/integral_trapezoid.h
@@ -1,1 +1,2 @@
 void integral_trapezoid_(const int *vecSize, const double xVector[], const double yVector[], const double *from, const double *to, double *result);
+double interp_(const double *x, const int *n, const double xVector[], const double yVector[]);
--- src/integral/integral_trapezoid.f
+++ src/integral/integral_trapezoid.f
@@ -3,7 +3,7 @@
         DIMENSION XVEC(IVSIZ), YVEC(IVSIZ)
 
         RESULT = 0
-        DO 20 I=1, IVSIZ-1
+        DO 20 I = 1, IVSIZ-1
           X0 = XVEC(I)
           X1 = XVEC(I + 1)
 
@@ -17,3 +17,24 @@
 
 C       PRINT *, RESULT
       END SUBROUTINE INTEGRAL_TRAPEZOID
+
+      FUNCTION INTERP(X, N, XV, YV)
+        IMPLICIT INTEGER*4 (I-N), DOUBLE PRECISION (A-H, O-Z)
+        DOUBLE PRECISION INTERP
+        DIMENSION XV(N), YV(N)
+
+        INTERP = 0
+
+        DO 40 I = 1, N
+
+          PROD = 1
+          DO 30 J = 1, N
+            IF (I.EQ.J) GOTO 30
+            PROD = PROD * (X - XV(J)) / (XV(I) - XV(J))
+30        CONTINUE
+
+        INTERP = INTERP + YV(I) * PROD
+
+40      CONTINUE
+
+      END FUNCTION
